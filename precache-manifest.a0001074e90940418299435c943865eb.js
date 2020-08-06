81e75fc774417a400ff21acce79f62d1bd200802
Chelsey Craig <chelsey.craig7321@gmail.com>
2020-08-06 09:49:00 -0400

    Updates

---
+++ precache-manifest.a0001074e90940418299435c943865eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "606c6b52341ace57fa797600a92e8846",
+    "url": "/User-Directory/index.html"
+  },
+  {
+    "revision": "c5d6faa231363e1ea879",
+    "url": "/User-Directory/static/css/main.1ef45aa2.chunk.css"
+  },
+  {
+    "revision": "752c5f2ca5b962f288f1",
+    "url": "/User-Directory/static/js/2.d1874a00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/User-Directory/static/js/2.d1874a00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5d6faa231363e1ea879",
+    "url": "/User-Directory/static/js/main.b9e11a81.chunk.js"
+  },
+  {
+    "revision": "976d1ad4fb8038d1b6f5",
+    "url": "/User-Directory/static/js/runtime-main.8b6464b0.js"
+  }
+]);
